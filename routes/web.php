9e8ce570e35e53a838ccf08d7e88f0f2dd3ab89c
Ben Meeder <ben.meeder@hudl.com>
2017-07-04 11:21:26 -0500

    Updates to forms and cleanup

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::get('/', function () {
     return view('index');
 });
 
-Route::get('/index', function() {
-    return view('index');
-});
-
 Route::get('/{id}', function($id) {
     return $id;
 });
